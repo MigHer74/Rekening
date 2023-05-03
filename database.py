c7cb7b528e8982789259f9f99bb574b74a5001ca
Miguel Hernandez <migher74@outlook.com>
2023-05-03 14:53:34 -0600

    Function to verify if the tables are created in the db.

--- database.py
+++ database.py
@@ -4,3 +4,12 @@ import sqlite3
 def conect_db():
     condb = sqlite3.connect("rekening.db")
     return condb
+
+def verify_data():
+    condb = conect_db()
+    curdb = condb.cursor()
+    scrsql = open("tables.sql")
+    scrvar = scrsql.read()
+    curdb.executescript(scrvar)
+    condb.commit()
+    condb.close()
