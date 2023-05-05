35bf2702d8393e95256fe2d1e0f4a92a816d9d9a
Miguel Hernandez <migher74@outlook.com>
2023-05-04 19:38:17 -0600

    Add set up option in the main menu.

--- menu.py
+++ menu.py
@@ -7,7 +7,7 @@ os.system("cls")
 while True:
     mheader = ["Menu Principal"]
     mdata = [["[1] - Beneficiarios"], ["[2] - Cheques"],
-             ["[3] - Bóveda"], [], ["[X] - Salir"]]
+             ["[3] - Bóveda"], ["[4] - Configuración"], [], ["[X] - Salir"]]
     mmenu = tbe.tabulate(mdata, mheader, tablefmt="simple_outline")
 
     print(mmenu)
