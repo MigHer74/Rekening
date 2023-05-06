3b832550d795891979d13ff5d0ac41a471ace053
Miguel Hernandez <migher74@outlook.com>
2023-05-05 22:22:00 -0600

    Initial configuration of the main window.

--- ui.py
+++ ui.py
@@ -1,1 +1,13 @@
 from tkinter import *
+
+# Main Window
+mw = Tk()
+mw.title("Rekening - Control de Cheques")
+mw.geometry("1000x700")
+mw.resizable(False, False)
+mw.configure(background="#f0f0f0")
+mwicon = PhotoImage(file="icon.png")
+mw.iconphoto(True, mwicon)
+
+
+mw.mainloop()
