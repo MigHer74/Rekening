73ec7b57cb7823562a63476dd648bc1c449161c3
Miguel Hernandez <migher74@outlook.com>
2023-05-05 20:33:13 -0600

    Setup screen first design for initial values for the system.

---
+++ screens.py
@@ -0,0 +1,11 @@
+import addons as adds
+import database as db
+
+idcom = 1
+reginit = 1
+
+
+print(f"Id Empresa: {idcom}")
+adds.enter_txt(1,50,"Tip - > Introduzca desde 1 hasta 50 letras.")
+print(f"Beneficiario Actual: {reginit}")
+adds.enter_txt(1,6,"Tip -> Introduzca desde 1 hasta 6 número")
