e4c88235b502c19fac0fffa5d545cb12f652ff62
Miguel Hernandez <migher74@outlook.com>
2023-05-06 20:15:59 -0600

    Added entry field to search values, the search button and the function of search value.

--- ui.py
+++ ui.py
@@ -1,5 +1,12 @@
 from tkinter import *
 
+
+def btnSearch():
+    print("Search Button Pressed")
+    dataSearch = mwes.get()
+    print("Value to search", dataSearch)
+
+
 # Main Window
 mw = Tk()
 mw.title("Rekening - Control de Cheques")
@@ -9,9 +16,17 @@ mw.configure(background="#f0f0f0")
 mwicon = PhotoImage(file="icon.png")
 mw.iconphoto(True, mwicon)
 
-# Main Windod Labels
+# Main Window Labels
 mwl1 = Label(mw, text="Buscar Beneficiario", font=(
     "Arial", 10, "bold")).place(x=25, y=25)
+
+# Main Window Buttons
+mwbs = Button(mw, text="Buscar", width=15,
+              command=btnSearch).place(x=550, y=25)
+
+# Main Window Entry
+mwes = Entry(mw, width=50)
+mwes.place(x=160, y=27)
 
 
 mw.mainloop()
