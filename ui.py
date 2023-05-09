7ac0d9f69cd662b9803b2a4e7151fda9f6708967
Miguel Hernandez <migher74@outlook.com>
2023-05-08 20:46:05 -0600

    Final design of the search entry field and search results listbox.

--- ui.py
+++ ui.py
@@ -1,6 +1,4 @@
 from tkinter import *
-from tkinter.ttk import Separator
-
 
 def btnSearch():
     print("Search Button Pressed")
@@ -16,34 +14,39 @@ mw.resizable(False, False)
 mwicon = PhotoImage(file="icon.png")
 mw.iconphoto(True, mwicon)
 
-# Main Window Labels
-mwl1 = Label(mw, text="Beneficiario", font=("Arial", 8, "bold")).place(x=25, y=7)
 
 # Main Window Buttons
-mwbs = Button(mw, text="Buscar", width=15, command=btnSearch).place(x=500, y=23)
+mwbs = Button(mw, text="Buscar", width=15,
+              command=btnSearch).place(x=500, y=23)
 
-# Main Window Entry
-mwes = Entry(mw, width=50)
-mwes.place(x=28, y=27)
 
-# Main Window ListBox
-mwf1 = Frame(mw)
-mws1 = Scrollbar(mwf1,orient=VERTICAL)
-mwlb1 = Listbox(mwf1,yscrollcommand=mws1.set,width=50,height=34,activestyle=NONE)
-mws1.config(command=mwlb1.yview)
-mws1.pack(side=RIGHT, fill=Y)
+# Main Window Entry & ListBox
+mwlf1 = LabelFrame(mw, text=" Receptor ")
+mwes = Entry(mwlf1, width=54).pack(padx=10, pady=5)
+mwlf1.pack(anchor="nw", padx=10, pady=10)
 
-for i in range(1,101):
-    mwlb1.insert(END,f"Elemento {i}")
+mwlf2 = LabelFrame(mw)
+mwlfs = Scrollbar(mwlf2, orient=VERTICAL, width=18)
+mwlfl = Listbox(mwlf2, yscrollcommand=mwlfs.set,
+                width=51, height=33, activestyle=NONE)
+mwlfs.config(command=mwlfl.yview)
+mwlfs.pack(side=RIGHT, fill=Y)
 
-mwlb1.pack()
-mwf1.place(x=28,y=55)
+for i in range(1, 101):
+    mwlfl.insert(END, f" Elemento {i} ")
+
+mwlfl.pack(padx=10, pady=15)
+mwlf2.place(x=10, y=56)
+
 
 # Main Window Footer
-mwft1 = Frame(mw,height=25,bg="#AF7AC5")
-mwft1.pack(fill="x",side="bottom")
-Label(mwft1,text="Desarrollado por Miguel Hernandez/MH Services", padx=7,bg="#AF7AC5",font=("Arial",7,"bold")).pack(side="left")
-Label(mwft1,text="2023", padx=7,bg="#AF7AC5",font=("Arial",7,"bold")).pack(side="right")
+mwft1 = Frame(mw, height=25, bg="#AF7AC5")
+mwft1.pack(fill="x", side="bottom")
+Label(mwft1, text="Desarrollado por Miguel Hernandez/MH Services",
+      padx=7, bg="#AF7AC5", font=("Arial", 7, "bold")).pack(side="left")
+Label(mwft1, text="2023", padx=7, bg="#AF7AC5",
+      font=("Arial", 7, "bold")).pack(side="right")
+
 
 # Main Window Loop
 mw.mainloop()
