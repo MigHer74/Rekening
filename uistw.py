297fecfb7ae8c72578b4b81ee785c866203e8013
Miguel Hernandez <migher74@outlook.com>
2023-05-10 14:50:55 -0600

    Initial settings of set up system window.

--- uistw.py
+++ uistw.py
@@ -1,1 +1,12 @@
 from tkinter import *
+
+stw = Tk()
+stw.title("Configuración")
+stw.geometry("600x210")
+stw.resizable(False, False)
+stwicon = PhotoImage(file="icon.png")
+stw.iconphoto(True, stwicon)
+
+
+# SetUp Window Loop
+stw.mainloop()
