65ce73b62f77f6171d48fa0e53bb97d4f4f4e6b5
Miguel Hernandez <migher74@outlook.com>
2023-05-10 14:55:28 -0600

    Labels and entries created for initial receptor id and initial document number.

--- uistw.py
+++ uistw.py
@@ -20,5 +20,17 @@ Label(stlf1, text="Empresa").place(x=10, y=50)
 Entry(stlf1, width=50).place(x=83, y=53)
 
 
+# SetUp Window General Information
+
+stlf2 = LabelFrame(stw, text=" Identificadores ", width=410, height=70)
+stlf2.place(x=15, y=125)
+
+Label(stlf2, text="Id Receptor").place(x=10, y=10)
+Entry(stlf2, width=10, state=DISABLED).place(x=82, y=12)
+
+Label(stlf2, text="Folio Cheque").place(x=250, y=10)
+Entry(stlf2, width=8).place(x=334, y=12)
+
+
 # SetUp Window Loop
 stw.mainloop()
