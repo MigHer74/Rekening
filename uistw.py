6ea631b64d08ac852eaa4030f890f00e33ad65b4
Miguel Hernandez <migher74@outlook.com>
2023-05-10 20:23:37 -0600

    The editor applied autopep8 format to the script.

--- uistw.py
+++ uistw.py
@@ -9,10 +9,10 @@ def win_setup():
     stwicon = PhotoImage(file="icon.png")
     stw.iconphoto(True, stwicon)
 
-
     # SetUp Window - General Information
 
-    stlf1 = LabelFrame(stw, text=" Información General ", width=410, height=105)
+    stlf1 = LabelFrame(stw, text=" Información General ",
+                       width=410, height=105)
     stlf1.place(x=15, y=10)
 
     Label(stlf1, text="Id Empresa").place(x=10, y=10)
@@ -20,7 +20,6 @@ def win_setup():
 
     Label(stlf1, text="Empresa").place(x=10, y=50)
     Entry(stlf1, width=50).place(x=83, y=53)
-
 
     # SetUp Window - Initial Values Receptor and Document
 
@@ -33,12 +32,11 @@ def win_setup():
     Label(stlf2, text="Folio Cheque").place(x=250, y=10)
     Entry(stlf2, width=8).place(x=334, y=12)
 
-
     # SetUp Window - Ok & Cancel Buttons
 
-    stbok = Button(stw,text="Aceptar",width=15).place(x=450,y=60)
-    stbcancel = Button(stw,text="Cancelar",width=15,state=DISABLED).place(x=450,y=110)
-
+    stbok = Button(stw, text="Aceptar", width=15).place(x=450, y=60)
+    stbcancel = Button(stw, text="Cancelar", width=15,
+                       state=DISABLED).place(x=450, y=110)
 
     # SetUp Window Loop
     stw.mainloop()
