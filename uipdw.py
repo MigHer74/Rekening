f5f7ed0d84115a9d42b34293b23c3a029ee38edf
Miguel Hernandez <migher74@outlook.com>
2023-05-11 20:51:23 -0600

    Initial settings of document print list window.

--- uipdw.py
+++ uipdw.py
@@ -1,1 +1,14 @@
 from tkinter import *
+
+
+pdlw = Tk()
+pdlw.title("Rekening - Cheques Por Imprimir")
+pdlw.geometry("700x575")
+pdlw.resizable(False, False)
+pdlwicon = PhotoImage(file="icon.png")
+pdlw.iconphoto(True, pdlwicon)
+
+
+# Print Document List Window Loop
+
+pdlw.mainloop()
