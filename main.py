5008f8d7576eb9eaab5360f0c0784ffaa1af68bd
Miguel Hernandez <migher74@outlook.com>
2023-05-15 22:10:31 -0600

    Main window function was inserted.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import database as db
 import uistw as st
+import uimw as mw
 
 
 os.system("cls")
@@ -12,4 +13,4 @@ is_company = db.verify_company()
 if not is_company:
     st.win_setup(True)
 
-print("Show the main window")
+mw.win_main()
