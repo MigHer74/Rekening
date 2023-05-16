8d5a29ecb9b30ca53eb7b2dc84059940e1380b88
Miguel Hernandez <migher74@outlook.com>
2023-05-15 20:27:53 -0600

    Variables declaration assign in each entry field.

--- uistw.py
+++ uistw.py
@@ -2,12 +2,28 @@ from tkinter import *
 
 
 def win_setup():
+    def oksetup():
+        pass
+
+    def nosetup():
+        stw.destroy()
+
     stw = Tk()
     stw.title("Rekening - Configuración")
     stw.geometry("580x210")
     stw.resizable(False, False)
     stwicon = PhotoImage(file="icon.png")
     stw.iconphoto(True, stwicon)
+
+    # SetUp Windows - Variables Declaration
+
+    entryval1 = IntVar()
+    entryval2 = StringVar()
+    entryval3 = StringVar()
+    entryval4 = IntVar()
+
+    entryval1.set(1)
+    entryval3.set("BEN-001")
 
     # SetUp Window - General Information
 
@@ -16,10 +32,11 @@ def win_setup():
     stlf1.place(x=15, y=10)
 
     Label(stlf1, text="Id Empresa").place(x=10, y=10)
-    Entry(stlf1, width=3, state=DISABLED).place(x=82, y=12)
+    Entry(stlf1, width=3, textvariable=entryval1,
+          state=DISABLED).place(x=82, y=12)
 
     Label(stlf1, text="Empresa").place(x=10, y=50)
-    Entry(stlf1, width=50).place(x=83, y=53)
+    Entry(stlf1, textvariable=entryval2, width=50).place(x=83, y=53)
 
     # SetUp Window - Initial Values Receptor and Document
 
@@ -27,16 +44,18 @@ def win_setup():
     stlf2.place(x=15, y=125)
 
     Label(stlf2, text="Id Receptor").place(x=10, y=10)
-    Entry(stlf2, width=10, state=DISABLED).place(x=82, y=12)
+    Entry(stlf2, textvariable=entryval3, width=10,
+          state=DISABLED).place(x=82, y=12)
 
     Label(stlf2, text="Folio Cheque").place(x=250, y=10)
-    Entry(stlf2, width=8).place(x=334, y=12)
+    Entry(stlf2, textvariable=entryval4, width=8).place(x=334, y=12)
 
     # SetUp Window - Ok & Cancel Buttons
 
-    stbok = Button(stw, text="Aceptar", width=15).place(x=450, y=60)
+    stbok = Button(stw, text="Aceptar", command=oksetup,
+                   width=15).place(x=450, y=60)
     stbcancel = Button(stw, text="Cancelar", width=15,
-                       state=DISABLED).place(x=450, y=110)
+                       command=nosetup).place(x=450, y=110)
 
     # SetUp Window Loop
     stw.mainloop()
