15696f7d2ff8487a8cfee7cab967549fe5ca86f3
Miguel Hernandez <migher74@outlook.com>
2023-05-16 22:07:32 -0600

    Initial settings of receptor information window.

--- uiriw.py
+++ uiriw.py
@@ -1,1 +1,14 @@
 from tkinter import *
+
+riw = Tk()
+#riw.transient()
+#riw.grab_set()
+riw.title("Rekening - Información del Receptor")
+riw.geometry("580x210")
+riw.resizable(False, False)
+riwicon = PhotoImage(file="icon.png")
+riw.iconphoto(True,riwicon)
+
+
+# Receptor Information - Window Loop
+riw.mainloop()
