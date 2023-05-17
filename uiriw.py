61d5bf3bae2356b3a25d09e0f6e1bd819a6c16e7
Miguel Hernandez <migher74@outlook.com>
2023-05-17 12:09:55 -0600

    Checkbutton for active state and buttons created.

--- uiriw.py
+++ uiriw.py
@@ -4,21 +4,28 @@ riw = Tk()
 # riw.transient()
 # riw.grab_set()
 riw.title("Rekening - Información del Receptor")
-riw.geometry("580x210")
+riw.geometry("480x165")
 riw.resizable(False, False)
 riwicon = PhotoImage(file="icon.png")
 riw.iconphoto(True, riwicon)
 
 # Receptor Information - LabelFrames, Labels, Entries & Checkbuttons
 
-riwlf = LabelFrame(riw, text=" Información General ", width=500, height=200)
+riwlf = LabelFrame(riw, text=" Información General ", width=458, height=95)
 riwlf.place(x=10, y=5)
 
 Label(riw, text="Id Receptor").place(x=17, y=30)
 Entry(riw, width=6, state=DISABLED).place(x=90, y=32)
 
 Label(riw, text="Nombre").place(x=17, y=60)
-Entry(riw, width=50).place(x=90, y=62)
+Entry(riw, width=60).place(x=90, y=62)
+
+Checkbutton(riw, text="Activo", onvalue=1, offvalue=0).place(x=394, y=32)
+
+# Receptor Information - Buttons
+
+Button(riw, text="Aceptar", width=15).place(x=70, y=120)
+Button(riw, text="Cancelar", width=15).place(x=300, y=120)
 
 # Receptor Information - Window Loop
 riw.mainloop()
