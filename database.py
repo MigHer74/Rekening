8208b6a4f53606d0b66a1f49f70a92fee0a5e7f5
Miguel Hernandez <migher74@outlook.com>
2023-05-17 20:03:22 -0600

    The function to insert data in the receptor table was created.

--- database.py
+++ database.py
@@ -38,3 +38,12 @@ def modify_company_info(valdb):
         "UPDATE 'company' SET com_name = ?, com_init_rec = ?, com_init_doc = ? WHERE id_com = 1", (valdb))
     condb.commit()
     condb.close()
+
+
+def insert_receptor(valdb):
+    condb = conect_db()
+    curdb = condb.cursor()
+    datdb = curdb.execute(
+        "INSERT INTO 'names' VALUES(id=?, name=?, active=?)", (valdb))
+    condb.commit()
+    condb.close()
