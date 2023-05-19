b684044feced409e5bcca856f228f8362f90cab8
Miguel Hernandez <migher74@outlook.com>
2023-05-18 19:08:14 -0600

    Conversion of the receiver window script into a function.

--- uiriw.py
+++ uiriw.py
@@ -2,73 +2,75 @@ from tkinter import *
 import database as db
 
 
-def okReceptor():
-    idrec = entryval1.get()
-    nomrec = entryval2.get()
-    starec = entryval3.get()
+def win_receiver(WS_status):
+    def okReceptor():
+        idrec = entryval1.get()
+        nomrec = entryval2.get()
+        starec = entryval3.get()
 
-    recnewid = []
-    datarec = (idrec, nomrec, starec)
+        recnewid = []
+        datarec = (idrec, nomrec, starec)
 
-    recserie = int(idrec[4:7])
-    recserie += 1
-    recserie = "BEN-" + str(recserie).zfill(3)
-    recnewid.append(recserie)
-    recnewid = tuple(recnewid)
+        recserie = int(idrec[4:7])
+        recserie += 1
+        recserie = "BEN-" + str(recserie).zfill(3)
+        recnewid.append(recserie)
+        recnewid = tuple(recnewid)
 
-    db.insert_receptor(datarec)
-    db.update_company_receptor(recnewid)
+        db.insert_receptor(datarec)
+        db.update_company_receptor(recnewid)
 
-    riw.destroy()
+        riw.destroy()
 
+    def noReceptor():
+        riw.destroy()
 
-def noReceptor():
-    riw.destroy()
+    def closeDisable():
+        pass
 
+    riw = Tk()
+    # riw.transient()
+    # riw.grab_set()
+    riw.title("Rekening - Información del Receptor")
+    riw.geometry("480x165")
+    riw.resizable(False, False)
+    riwicon = PhotoImage(file="icon.png")
+    riw.iconphoto(True, riwicon)
+    riw.protocol("WM_DELETE_WINDOW", closeDisable)
 
-def closeDisable():
-    pass
+    # Receptor Information - Variable Declaration
 
+    entryval1 = StringVar()
+    entryval2 = StringVar()
+    entryval3 = IntVar()
 
-riw = Tk()
-# riw.transient()
-# riw.grab_set()
-riw.title("Rekening - Información del Receptor")
-riw.geometry("480x165")
-riw.resizable(False, False)
-riwicon = PhotoImage(file="icon.png")
-riw.iconphoto(True, riwicon)
-riw.protocol("WM_DELETE_WINDOW", closeDisable)
+    dc = db.verify_company()
 
-# Receptor Information - Variable Declaration
+    entryval1.set(dc[0][2])
+    entryval3.set(1)
 
-entryval1 = StringVar()
-entryval2 = StringVar()
-entryval3 = IntVar()
+    # Receptor Information - LabelFrames, Labels, Entries & Checkbuttons
 
-dc = db.verify_company()
+    riwlf = LabelFrame(riw, text=" Información General ",
+                       width=458, height=95)
+    riwlf.place(x=10, y=5)
 
-entryval1.set(dc[0][2])
-entryval3.set(1)
+    Label(riw, text="Id Receptor").place(x=17, y=30)
+    Entry(riw, width=8, textvariable=entryval1,
+          state=DISABLED).place(x=90, y=32)
 
-# Receptor Information - LabelFrames, Labels, Entries & Checkbuttons
+    Label(riw, text="Nombre").place(x=17, y=60)
+    Entry(riw, textvariable=entryval2, width=60).place(x=90, y=62)
 
-riwlf = LabelFrame(riw, text=" Información General ", width=458, height=95)
-riwlf.place(x=10, y=5)
+    Checkbutton(riw, text="Activo", variable=entryval3,
+                onvalue=1, offvalue=0).place(x=394, y=32)
 
-Label(riw, text="Id Receptor").place(x=17, y=30)
-Entry(riw, width=8, textvariable=entryval1, state=DISABLED).place(x=90, y=32)
+    # Receptor Information - Buttons
 
-Label(riw, text="Nombre").place(x=17, y=60)
-Entry(riw, textvariable=entryval2, width=60).place(x=90, y=62)
+    Button(riw, text="Aceptar", width=15,
+           command=okReceptor).place(x=70, y=120)
+    Button(riw, text="Cancelar", width=15,
+           command=noReceptor).place(x=300, y=120)
 
-Checkbutton(riw, text="Activo", variable=entryval3,
-            onvalue=1, offvalue=0).place(x=394, y=32)
-
-# Receptor Information - Buttons
-
-Button(riw, text="Aceptar", width=15, command=okReceptor).place(x=70, y=120)
-Button(riw, text="Cancelar", width=15, command=noReceptor).place(x=300, y=120)
-
-# Receptor Information - Window Loop
-riw.mainloop()
+    # Receptor Information - Window Loop
+    riw.mainloop()
